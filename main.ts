5f7f5ddf1f90e33863aee757ce7e0bd1df9cf1b4
Michael Ly <105381067+magile32@users.noreply.github.com>
2023-09-19 09:19:24 -0700

    Implement binary search function and add more tests
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Fritz Madden <fmadden@flexion.us>
    Co-authored-by: Arthur Murrow <amurrow@flexion.us>

--- main.ts
+++ main.ts
@@ -1,9 +1,35 @@
 // karate-chop kata
-const chopArray:number[] = [];
-for (let i: number=0; i<100; i++){
-  chopArray.push(i);
-}
+// const chopArray:number[] = [];
+// for (let i: number=0; i<100; i++){
+//   chopArray.push(i);
+// }
 
 export function binarySearch(arr: number[], val: number): number {
-  return 0;
+
+  if (arr.length === 0 ) {
+    return -1;
+  }
+let midIndex;
+  if (arr.length===1){
+    midIndex=0;
+  } else {
+    midIndex = Math.floor(arr.length / 2.0);
+  }
+  const middleValue = arr[midIndex];
+  const left = arr.slice(0, midIndex);
+  const right = arr.slice(midIndex, arr.length);
+
+  console.log(left);
+  console.log(right);
+  console.log(midIndex);
+  if (middleValue === val) {
+    return midIndex;
+  } else if (arr.length === 1) {
+    return -1;
+  }
+  else if (middleValue > val) {
+    return binarySearch(left, val);
+  } else  {
+    return binarySearch(right, val);
+  }
 }
